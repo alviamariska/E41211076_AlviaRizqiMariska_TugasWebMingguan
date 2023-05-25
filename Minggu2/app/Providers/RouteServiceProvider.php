c77826b34ba31d6294b8b78513eac28210d2f8bc
alviamariska <alviamariska2@gmail.com>
2023-05-25 14:22:03 +0700

    Minggu2

--- Minggu2/app/Providers/RouteServiceProvider.php
+++ Minggu2/app/Providers/RouteServiceProvider.php
@@ -24,6 +24,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Route::pattern('name','[A-Zz-Z]+');
+        parent::boot();
+        
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
--- Minggu2/routes/web.php
+++ Minggu2/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,93 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+}); //route default
+
+Route::get('/login', function () {
+    return view('login');
+}); //route tmpilan login
+
+Route::POST('/logins', function (Request $request) {
+    dd($request);
+});
+
+Route::get('/there', function(){
+    return "ini halaman untuk latihan route redirect";
+});
+
+Route::view('/welcome', 'welcome', ['tittle' => 'Alvia Rizqi Mariska']);
+
+Route::get('users/{id}', function($id){
+    return 'Welcome ' .$id;
+ });
+
+ Route::get('users/{id?}', function($id = null){
+    return 'Welcome ' .$id;
+ });
+
+ Route::get('pengguna/{id?}', function($id = 'Alvia Rizqi Mariska'){
+    return 'Welcome ' .$id;
+ });
+
+ Route::get('names/{name?}', function($name){
+    return 'Welcome ' .$name;
+ })->where('name','[A-Za-z]+');
+
+ Route::get('names/{name?}', function($name){
+    return 'Welcome ' .$name;
+ })->where('name', '.*');
+
+ Route::view('/login', 'login', ['tittle' => 'Login Page'])
+ ->name('login');
+
+Route::get('/user', function() {
+    $url = route('users');
+})->where('id', '.*');
+
+Route::get('users/{id?}', function ($id = "Alvia") {
+    return "Welcome " .$id;
+})->where('name', '.*')->name('users');
+
+Route::get('/user', function($id = "Alvia"){
+    $url = route('users', [
+        'id' => $id,
+    ]);
+    return redirect()->route('users',[
+        'id' => $id,
+    ]);
+})->where('id', '.*');
+
+Route::prefix('admin')->middleware(['auth','admin'])->group(function(){
+    Route::get('/users', function(){
+        return 'ini halaman /admin/users';
+    });
+    Route::get('/home', function(){
+        return 'ini halaman /admin/home';
+    });
+    Route::get('/report', function(){
+        return 'ini halaman /admin/report';
+    });
+});
+
+Route::namespace('Admin')->group(function(){
+    Route::get('Home', 'HomeController@index');
+    Route::get('Report', 'ReportController@index');
+});
+
+Route::domain('Alvia.mayapp.com')->group(function(){
+    Route::get('users/{id}', function(){
+        return 'coba';
+    });
+});
+
+Route::prefix('admin')->group(function(){
+    Route::get('/users', function(){
+        return 'ini halaman /admin/users';
+    });
+    Route::get('/home', function(){
+        return 'ini halaman /admin/home';
+    });
+    Route::get('/report', function(){
+        return 'ini halaman /admin/report';
+    });
 });
