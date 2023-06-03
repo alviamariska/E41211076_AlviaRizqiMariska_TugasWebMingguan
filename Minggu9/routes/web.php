4a9888a5f16813791512df75e317355e55037491
alviamariska <alviamariska2@gmail.com>
2023-06-03 19:17:17 +0700

    m9

--- Minggu9/routes/web.php
+++ Minggu9/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\backend\DashboardController;
+use App\Http\Controllers\PendidikanController;
+use App\Http\Controllers\pengalaman_kerja;
+use App\Models\Pendidikan;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +13,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::resource('/',HomeController::class);
+
+Route::resource('dashboard',DashboardController::class);
+// untuk menuju ke halaman pengelaman kerja
+Route::resource('pengalaman_kerja',pengalaman_kerja::class);
+// untuk menuju ke halaman pendidikan
+Route::resource('pendidikan',PendidikanController::class);
+
+
+
